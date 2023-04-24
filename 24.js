c7f60b1a98debcef93712d508096ce54f1c0006f
codewithfaizan <faizanoic.com@gmail.com>
2023-04-24 23:56:13 +0530

    javascript

---
+++ 24.js
@@ -0,0 +1,16 @@
+// let capitalize = (string) =>{
+//     string = string.charAt(0).toUpperCase() + string.slice(1)
+//     return string
+// }
+
+// let result = capitalize("hinder")
+// console.log(result)
+
+
+// 
+
+// let findVowel = (string) => {
+//     let count = string.match(/[aeiou]/ig).length;
+//     return count ;
+// }
+// console.log(findVowel("Marvelous"))
---
+++ suhail.js
@@ -0,0 +1,16 @@
+let fac = (input) => {
+  // input = 5
+  //
+
+  let sum = 1;
+ for(let i = input;i<=input;i--)
+ {
+ 
+   sum *= input
+
+}
+console.log(sum);
+};
+
+
+fac(5);
