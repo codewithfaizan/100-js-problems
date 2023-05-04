a3c900038a27cfc4b275cec6a274c5156b1742aa
codewithfaizan <faizanoic.com@gmail.com>
2023-05-04 23:22:13 +0530

    /

--- async.js
+++ async.js
@@ -1,13 +1,13 @@
 function resolveAfter2Seconds() {
-    return new Promise(resolve => {
-      setTimeout(() => {
-        resolve('resolved');
-      }, 2000);
-    });
+    return new Promise( 
+        resolve => {
+      setTimeout(() => { resolve('resolved after 2 sec') }, 2000); 
+    } );
   }
   
   async function asyncCall() {
     console.log('calling');
+
     const result = await resolveAfter2Seconds();
     console.log(result);
     // Expected output: "resolved"
