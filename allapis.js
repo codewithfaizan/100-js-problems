eb4fc4f85cb4ecff18ea8998d24028bf4f09196c
codewithfaizan <faizanoic.com@gmail.com>
2023-05-15 20:51:48 +0530

    GET

--- allapis.js
+++ allapis.js
@@ -1,9 +1,15 @@
 async function getdata(){
- const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+ const response = await fetch(('https://jsonplaceholder.typicode.com/todos/1'), {
+    method : 'GET' ,
+    headers :{
+        application : application/json;
+    }
+})
  .then(response => response.json())
 
  .then(json => console.log(json))
- 
+
  return response.json();
 }
+
 getdata();
